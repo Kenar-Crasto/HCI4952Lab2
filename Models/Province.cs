8b2d5923a0e4d455bc9883580aa4ce2f92233af7
kenarrr <itskenar@gmail.com>
2023-09-28 11:02:19 -0700

    shoutout miguel

--- Models/Province.cs
+++ Models/Province.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace CommunityApp.Models
         [Key]
         public string? ProvinceCode {get;set;}
         public string? ProvinceName{get;set;}
+        [InverseProperty("Province")]
         public List<City>? Cities{get;set;}
 
         
--- Pages/ProvincePages/Details.cshtml.cs
+++ Pages/ProvincePages/Details.cshtml.cs
@@ -21,23 +21,34 @@ namespace CommunityApp.Pages_ProvincePages
 
       public Province Province { get; set; } = default!; 
 
-        public async Task<IActionResult> OnGetAsync(string id)
+public async Task<IActionResult> OnGetAsync(string id)
+{
+    try
+    {
+        if (id == null || _context.Province == null)
         {
-            if (id == null || _context.Province == null)
-            {
-                return NotFound();
-            }
-
-            var province = await _context.Province.FirstOrDefaultAsync(m => m.ProvinceCode == id);
-            if (province == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Province = province;
-            }
-            return Page();
+            return NotFound();
         }
+
+        var province = await _context.Province
+            .Include(p => p.Cities)
+            .FirstOrDefaultAsync(m => m.ProvinceCode == id);
+
+        if (province == null)
+        {
+            return NotFound();
+        }
+
+        Province = province;
+        return Page();
+    }
+    catch (Exception ex)
+    {
+        // Log the exception for debugging purposes
+        // You can also return an error page or message to the user
+        return Content("An error occurred: " + ex.Message);
+    }
+}
+
     }
 }
